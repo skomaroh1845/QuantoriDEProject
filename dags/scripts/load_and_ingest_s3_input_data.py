52d39f0abead17fc5142f26fcd0514b253bfa626
skomaroh1845 <komarov.1845@gmail.com>
2024-07-07 20:57:06 +0300

    Feat Added wrong mols names handling

--- dags/scripts/load_and_ingest_s3_input_data.py
+++ dags/scripts/load_and_ingest_s3_input_data.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import re
+
 import pandas as pd
 
 from airflow.hooks.S3_hook import S3Hook
@@ -10,7 +12,19 @@ from airflow.models import (
 )
 
 
-def rename_file(name: str, new_name: str) -> None:
+def _fix_name(mol_name: str) -> str:
+    # check if name correct
+    if re.search(r'^CHEMBL[0-9]+$', mol_name): 
+        return mol_name
+    # check mol name pattern CHEMBL1234
+    if re.search(r'.*[a-zA-ZäöüßÄÖÜ]{1}[0-9]+$', mol_name):  
+        num_start = re.search(r'[0-9]+', mol_name).span()[0]
+        if mol_name[num_start:].isnumeric():
+            return 'CHEMBL' + mol_name[num_start:]
+    # return None if name can not be fixed
+    return None
+
+def _rename_file(name: str, new_name: str) -> None:
     file_path = '/'.join(name.split('/')[:-1])
     new_path = f'{file_path}/{new_name}'
     os.rename(src=name, dst=new_path)
@@ -33,7 +47,7 @@ def load_s3_input_data(ti) -> None:
     input_file_names = []
     for key in list_of_input_files:
         file_name = s3_hook.download_file(key, bucket_name, path_to_download)
-        input_file_names.append(rename_file(file_name, key.split('/')[-1]))
+        input_file_names.append(_rename_file(file_name, key.split('/')[-1]))
         logging.info(f'Saved: {input_file_names[-1]}')
 
     ti.xcom_push(key='input_file_paths', value=input_file_names)
@@ -43,13 +57,28 @@ def load_s3_input_data(ti) -> None:
 def ingest_s3_input_data(ti) -> None:
     input_file_paths = ti.xcom_pull(task_ids='load_s3_input_data', key='input_file_paths')
 
+    # read data
     # files are quite small so I read them all at ones
-    df = pd.concat([pd.read_csv(path, on_bad_lines='skip', encoding='latin-1') for path in input_file_paths])
+    df_list = []
+    for f in input_file_paths:
+        try:
+            tmp_df = pd.read_csv(f, on_bad_lines='skip')
+        except UnicodeDecodeError:
+            # change encoding
+            tmp_df = pd.read_csv(f, on_bad_lines='skip', encoding='latin-1')
+
+        # change col names
+        tmp_df.columns = ['chembl_id', 'smile']
+
+        df_list.append(tmp_df)
+
+    df = pd.concat(df_list).reset_index(drop=True)
     
-    df = df.rename(
-        lambda column_name: column_name.lower().replace(' ', '_').replace('#', '_').strip('_'),
-        axis='columns'
-    )
+    # check and fix molecules names
+    df.chembl_id = df.chembl_id.apply(_fix_name)
+
+    # drop invalid names
+    df = df.dropna()
 
     logging.info('Starting ingestion into database...')
 
